5f132c494b036f0c00d4a4bd2dd675bdb8623530
fstar1129 <fstar0550@gmail.com>
2022-08-04 06:49:27 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,216 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const path = require('path')
+
+const PORT = process.env.PORT || 5000
+
+//global game state
+const game = {
+  users: [],
+  observers: [],
+  reveal: false,
+  previousBid: {
+    player: null,
+    diceVal: 2,
+    diceAmt: 0
+  } 
+};
+
+function getUserCount() {
+  let count = game.users.length;
+  return count;
+}
+
+function isGameOver() {
+  let users = game.users;
+  let usersWithDice = 0;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].dice.length >= 1) {
+      usersWithDice ++;
+    }
+  }
+  if ((usersWithDice <= 1) && (users.length > 1))  {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// returns newly created random array of ints
+function playersDice(diceCount) {
+  let arr = [];
+  for (let i = 0; i < diceCount; i++ ) {
+    let roll = Math.ceil(Math.random() * 6);
+    arr.push(roll);
+  }
+  return arr;
+}
+
+function diceAll() {
+  let count = 0;
+  for (let i = 0; i < game.users.length; i++) {
+    count = count + game.users[i].dice.length;
+  }
+  return count;
+}
+
+function anonymize(diceCount) {
+  let arr = [];
+  for (let i = 0; i < diceCount; i++) {
+    arr.push('0');
+  }
+  return arr;
+}
+
+// returns a newly created user object
+function createUserHandler(req, res) {
+  let displayName = req.query.displayName;
+  let userID = Math.random().toString();
+  let newUser = {
+    id: userID,
+    dice: playersDice(5),
+    name: displayName
+  };
+  game.users.push(newUser);
+  let response = newUser;
+  res.send(response);
+}
+
+function rootHandler(req, res) {
+  res.render('pages/index');
+}
+
+function isIt(userID) {
+  if (game.users.length > 1) {
+    let itUser = game.users[1];
+    if (itUser.id == userID) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+function getGameForUser(userID) {
+  let obs = userToObserver(game);
+  let privateGame = {
+    users: [],
+    observers: obs,
+    isOver: isGameOver(),
+    diceTotal: diceAll(),
+    previousBid: game.previousBid,
+    isIt: isIt(userID)
+  }; 
+  for (var i = 0; i < getUserCount(); i++) {
+    let user = game.users[i];
+    if ((user.id == userID) || game.reveal) {
+      privateGame.users.push(game.users[i]);
+    } else {
+      let otherUser =  {
+        dice: anonymize(game.users[i].dice.length), 
+        name: user.name
+      };
+      privateGame.users.push(otherUser);
+    }
+  }
+  return privateGame;
+}
+
+function userToObserver(game) {
+  for (let i = 0; i < getUserCount(); i++) {
+    let outOfGame;
+    if (game.users[i].dice.length == 0) {
+      outOfGame = game.users[i];
+      game.observers.push(outOfGame);
+      game.users.splice([i], 1);
+    }
+  }
+  return game.observers;
+}
+
+function gamesHandler(req, res) {
+  let userID = req.query.userID;
+  let myGame = getGameForUser(userID);
+  let response = {
+    game: myGame
+  };
+  res.send(response);
+}
+
+function reroll() {
+  for (let i = 0; i < getUserCount(); i++) {
+    game.users[i].dice = playersDice(game.users[i].dice.length);
+  }
+  game.reveal = false;
+}
+
+function revealHandler() {
+  game.reveal = true;
+}
+
+function advanceItUser() {
+  let arr = game.users;
+  arr.push(arr.shift());
+}
+
+function newGameHandler(req, res) {
+  for (var i = 0; i < getUserCount(); i++) {
+    let user = game.users[i];
+    let userID = req.query.userID;
+    user.dice = playersDice(5);
+    if (user.id == userID) {
+      revealHandler();
+    } 
+  res.send();
+  }
+}
+
+function checkBid() {
+  let users = game.users;
+  let n = 0;
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < users[i].dice.length; j++) {
+      if ((users[i].dice[j] == game.previousBid.diceVal) || (users[i].dice[j] == 1)) {
+        console.log("users i dice j " + users[i].dice[j]);
+        console.log("pbid " + game.previousBid.diceVal);
+        n = n + 1;
+        console.log("n " + n);
+      }
+    }
+  }
+  if (n >= game.previousBid.diceAmt) {
+    users[1].dice.pop();
+    advanceItUser()
+  } else {
+    users[0].dice.pop();
+  }
+  game.previousBid.diceAmt = 0;
+  game.previousBid.diceVal = 2;
+  reroll();
+}
+
+function removeHandler(req, res) {
+  revealHandler();
+  setTimeout(checkBid, 5000);
+  res.send();
+}
+function bidHandler(req, res) {
+  game.previousBid = req.body.latestBid;
+  res.send(game.previousBid);
+  advanceItUser();
+}
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', rootHandler)
+  .post('/users', createUserHandler)
+  .get('/games', gamesHandler)
+  .post('/games', newGameHandler)
+  .delete('/users/dice', removeHandler)
+  .post('/bids', bidHandler)
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ src/index.js
@@ -0,0 +1,383 @@
+// This file is the root of the javascript that runs in the browser
+// This gets transpiled into bundle.js
+// Access this code via the variable name EntryPoint (ex: EntryPoint.getGame)
+
+const axios = require('axios');
+
+let myID;
+let gameState;
+let userAmt;
+let displayName;
+let isOver;
+let previousBid = {
+  player: '',
+  diceVal: 0,
+  diceAmt: 0
+};
+let latestBid = {
+  player: '',
+  diceVal: 2,
+  diceAmt: 0
+};
+
+// returns a Promise
+function getGame(userID) {
+  return axios.get('/games?userID=' + userID);
+}
+
+function createUser(displayName) {
+  return axios.post('/users?displayName=' + displayName);
+}
+
+function reveal() {
+  axios.delete('/users/dice')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  latestBid.diceAmt = 1;
+  latestBid.diceVal = 2;
+}
+
+function bid() {
+  axios.post('/bids', {
+    latestBid: latestBid 
+  })
+  .then(function (response) {
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+}
+
+function newGame() {
+  axios.post('/games')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function showPlayButtons(isIt) {
+  if (isIt == false) {
+    document.getElementById('reveal').style.visibility = 'hidden';
+  } else {
+    document.getElementById('reveal').style.visibility = 'visible';
+  }
+}
+
+function showNewGameButton() {
+  document.getElementById('newGame').style.visibility = 'visible';
+}
+
+function displayBidEngine(isIt) {
+  if (isIt == false) {
+    document.getElementById('bidengine').style.visibility = 'hidden';
+  } else {
+    document.getElementById('bidengine').style.visibility = 'visible';
+  }
+}
+
+function update(userID) {
+  getGame(userID).then(response => {
+    gameState = response.data.game;
+    observeState = response.data.game.observers;
+    isOver = response.data.game.isOver;
+    previousBid = response.data.game.previousBid;
+    isIt = response.data.game.isIt;
+    userAmt = response.data.game.users.length;
+    if (isOver) {
+      showNewGameButton();
+    } else {
+      showPlayButtons(isIt);
+    }
+    displayBidEngine(isIt);
+    drawGame(gameState, myID);
+    drawObservers(observeState)
+    reDrawPreviousBid();
+    setTimeout(update, 1000, myID);
+  });
+}
+
+function reDrawPreviousBid() {
+  let doc = window.document;
+  
+  let dA = previousBid.diceAmt;
+  let diceBidDisplay = doc.getElementById('dicedisplay');
+  diceBidDisplay.textContent = 'Bid: ' + dA; 
+
+  let dV = previousBid.diceVal;
+  let diceValue = doc.getElementById('biddiceface');
+  diceValue.setAttribute('src', '/images/' + dV + '.gif');
+
+  drawBid()
+}
+
+function initialize() {
+  drawButtons();
+  drawPreviousBid();
+  drawBid();
+  displayName = prompt("Type your name and click OK", "New Player");
+  if (displayName == "") {
+    displayName = "New Player";
+  } 
+  createUser(displayName).then(response => {
+    myID = response.data.id;
+    update(myID);
+  });
+}
+
+function getImages(roll) {
+  let arr = [];
+  for (let i = 0; i < roll.length; i++) {
+    if (roll[i] === 0) {
+      arr.push('0.gif');
+    } else {
+      arr.push(roll[i] + '.gif');
+    }
+  }
+  return arr;
+} 
+
+function incrementDiceVal() {
+  if (latestBid.diceVal == 0) {
+    latestBid.diceVal = 3;
+  } else if (latestBid.diceVal < 6) {
+    latestBid.diceVal++;
+  } 
+  drawBid();
+}
+
+function decrementDiceVal() {
+  if (latestBid.diceVal > 2) {
+    latestBid.diceVal--;
+  } 
+  drawBid();
+}
+
+function increaseDice() {
+  let diceNo = latestBid.diceAmt;
+  if (diceNo < gameState.diceTotal) {
+    diceNo = diceNo +1;
+  } else {
+    diceNo;
+  }
+  latestBid.diceAmt = diceNo;
+  drawBid();
+}
+
+function decreaseDice() {
+  let diceNo = latestBid.diceAmt;
+  if (diceNo == 1) {
+    diceNo;
+  } else {
+    diceNo = diceNo -1;
+  }
+  latestBid.diceAmt = diceNo;
+  drawBid();
+}
+
+function createImage(imagePath, height, width, onClickFunction, parent) {
+  let image = window.document.createElement('img');
+    image.setAttribute('src', imagePath); 
+    image.setAttribute('height', height);
+    image.setAttribute('width', width);
+    image.onclick = onClickFunction; 
+    parent.appendChild(image);
+    return image;
+}
+
+function drawGame(gameState, myID) {
+  let doc = window.document;
+  let gameDiv = document.getElementById('game');
+  while (gameDiv.firstChild) {
+    gameDiv.removeChild(gameDiv.firstChild);
+  }
+  let userCount = gameState.users.length;
+  let diceCountDiv = doc.createElement('div');
+  diceCountDiv.className = 'diceCount';
+  gameDiv.appendChild(diceCountDiv);
+  diceCountDiv.textContent = "There are  " + gameState.diceTotal + " dice in the game"; 
+  for (let i = 0; i < userCount; i++) {
+    let roll = gameState.users[i].dice;
+    let name = gameState.users[i].name;
+    let player = doc.createElement('div');   
+    if (i == 1) { 
+        player.className = 'it';
+        player.textContent = name + ":  ";
+        let imageRoll = getImages(roll);
+        for (let i = 0; i < imageRoll.length; i++) {
+        diceValue = roll[i] + '.gif';
+        let diceImg = doc.createElement('img');        
+        diceImg.setAttribute('src', '/images/' + diceValue);
+        diceImg.setAttribute('height', '32');
+        diceImg.setAttribute('width', '32');
+        document.getElementsByClassName('it');
+        player.appendChild(diceImg);
+        }
+      } else {
+          player.className = 'player';
+          player.textContent = name + ":  ";
+          let imageRoll = getImages(roll);
+          for (let i = 0; i < imageRoll.length; i++) {
+          diceValue = roll[i] + '.gif';
+          let diceImg = doc.createElement('img');        
+          diceImg.setAttribute('src', '/images/' + diceValue);
+          diceImg.setAttribute('height', '32');
+          diceImg.setAttribute('width', '32');
+          document.getElementsByClassName('player');
+          player.appendChild(diceImg);
+          }
+        }  
+    gameDiv.appendChild(player); 
+    }
+  }
+  
+  function drawObservers(observeState) {
+    let doc = window.document;
+    let observerDiv = document.getElementById('observer');
+    while (observerDiv.firstChild) {
+      observerDiv.removeChild(observerDiv.firstChild);
+    }
+    for (let i = 0; i < observeState.length; i++) {
+      let observer = doc.createElement('div');  
+      let name = observeState[i].name;
+      observer.className = 'observer';
+      observer.textContent =  name + ":  is watching";
+      observerDiv.appendChild(observer);
+    }
+  }
+
+  function drawPreviousBid() {
+    let doc = window.document;
+    let pBidDiv = doc.getElementById('previousbid');
+    while (pBidDiv.firstChild) {
+      pBidDiv.removeChild(pBidDiv.firstChild);
+    }
+    let mainCont = doc.getElementById('main');
+    let bidCont = doc.getElementById('bids');
+    let pBidDiceVal = doc.createElement('div');
+    let pBidDiceAmt = doc.createElement('div');
+    pBidDiceVal.className = 'pBidDiceVal';
+    pBidDiceAmt.className = 'pBidDiceAmt';
+    mainCont.appendChild(bidCont);
+    bidCont.appendChild(pBidDiv);
+    pBidDiv.appendChild(pBidDiceAmt);
+    pBidDiv.appendChild(pBidDiceVal);
+    pDVal = previousBid.diceVal;
+
+    let diceBidDisplay = doc.createElement('p');
+    diceBidDisplay.setAttribute('id', 'dicedisplay');
+    let diceBidNumber = previousBid.diceAmt;
+    diceBidNumber.toString();
+    pBidDiceAmt.appendChild(diceBidDisplay);
+    diceBidDisplay.textContent = 'Bid: ' + diceBidNumber;
+
+    let diceValue = doc.createElement('img');
+    diceValue.setAttribute('id', 'biddiceface');
+    diceValue.setAttribute('src', '/images/' + pDVal + '.gif');
+    diceValue.setAttribute('height', '32');
+    diceValue.setAttribute('width', '32');
+    pBidDiceVal.appendChild(diceValue);
+
+    let revealButton = doc.createElement('button');
+
+    pBidDiv.appendChild(revealButton);
+    revealButton.id = 'reveal';
+    revealButton.textContent = " Liar! ";
+    revealButton.href = "#";
+    revealButton.onclick = reveal;
+  }
+  
+  function drawBid() {
+    let doc = window.document;
+    let bidDiv = doc.getElementById('bidengine');
+    while (bidDiv.firstChild) {
+      bidDiv.removeChild(bidDiv.firstChild);
+    }
+      let mainCont = doc.getElementById('main');
+      let bidCont = doc.getElementById('bids');
+      let bidDiceVal = doc.createElement('div');
+      bidDiceVal.className = 'bidDiceVal';
+      let bidDiceAmt = doc.createElement('div');
+      bidDiceAmt.className = 'bidDiceAmt';
+      let bidBut = doc.createElement('div');
+      mainCont.appendChild(bidCont);
+      bidCont.appendChild(bidDiv);
+      bidDiv.appendChild(bidDiceAmt);
+      bidDiv.appendChild(bidDiceVal);
+      bidDiv.appendChild(bidBut);
+
+      createImage('/images/uparrow.gif', '16', '32', incrementDiceVal, bidDiceVal);
+      let br1 = doc.createElement('br');
+      bidDiceVal.appendChild(br1); 
+
+      dVal = latestBid.diceVal;
+      let diceValue = doc.createElement('img');
+      diceValue.setAttribute('id', 'biddiceface');
+      diceValue.setAttribute('src', '/images/' + dVal + '.gif');
+      diceValue.setAttribute('height', '32');
+      diceValue.setAttribute('width', '32');
+      document.getElementById('bidengine');
+      bidDiceVal.appendChild(diceValue);
+      let br2 = doc.createElement('br');
+      bidDiceVal.appendChild(br2);
+
+      createImage('/images/downarrow.gif', '16', '32', decrementDiceVal, bidDiceVal); 
+      let br3 = doc.createElement('br');
+      bidDiceVal.appendChild(br3);
+
+      createImage('/images/uparrow.gif', '16', '32', increaseDice, bidDiceAmt);
+      let br4 = doc.createElement('br');
+      bidDiceAmt.appendChild(br4);
+
+      let diceBidDisplay = doc.createElement('p');
+      let diceBidV = latestBid.diceAmt;
+      diceBidV.toString();
+      bidDiceAmt.appendChild(diceBidDisplay);
+      diceBidDisplay.textContent = diceBidV;
+      let br5 = doc.createElement('br');
+      bidDiceAmt.appendChild(br5);
+
+      createImage('/images/downarrow.gif', '16', '32', decreaseDice, bidDiceAmt);
+      
+      let bidButton = doc.createElement('button');
+      bidBut.appendChild(bidButton);
+      bidButton.id = 'bid';
+      bidButton.textContent = " Place Bid ";
+      bidButton.href = "#";
+      bidButton.onclick = bidBtn;
+
+  }
+
+  function validateBid() {
+    if ((latestBid.diceAmt > previousBid.diceAmt) || ((latestBid.diceVal > previousBid.diceVal) && (latestBid.diceAmt >= previousBid.diceAmt))) {
+      bid();
+    } else {
+      alert("Your bid is not valid. Try again.")
+    }
+  }
+    
+  function bidBtn() {
+    validateBid();
+  }
+
+  function drawButtons() {
+  let doc = window.document;
+  
+  let newGameButton = doc.createElement('button');
+  doc.body.appendChild(newGameButton);
+  newGameButton.id = 'newGame';
+  newGameButton.style.visibility = 'hidden';
+  newGameButton.textContent = " Play Again? ";
+  newGameButton.href = "#";
+  newGameButton.onclick = newGame;
+}
+
+module.exports = {
+  initialize: initialize
+}
